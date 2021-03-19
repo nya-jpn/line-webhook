ad7d6f64c4dc3c80a8e078dae4ffcb1ae0fd8d8b
nya <yuki.nakamura.jpn@gmail.com>
2021-03-20 00:19:52 +0900

    remove secrets

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -4,11 +4,14 @@ import * as cdk from '@aws-cdk/core';
 import { WebhookStack } from '../lib/webhook';
 import { join } from 'path'
 
+require('dotenv').config()
+
+
 const lambdaLayerDir = join(process.cwd(), '../cdk/dist/layer')
 const lambdaCodeDir = join(process.cwd(), '../cdk/lambda')
 
-const lineChannelAccessToken = 'A6fdHeS8is6J4zXRxcljRYCvBRaw0/ImWeaRJEfLZ3x8tLpgxuigFCubsbeML3B1U2wLmhTe/jzsMWKLemtc1NAkCLA79oBDerbBFqHz1ZBooU6q2sHMsPt8t7T5E6CQuQoG9A+KWcgT2RhzwNfSJwdB04t89/1O/w1cDnyilFU='
-const lineChannelSecret = 'e25222e555ea1faacec6feb52256b279'
+const lineChannelAccessToken = process.env.LINE_CHANNEL_ACCESS_TOKEN!
+const lineChannelSecret = process.env.LINE_CHANNEL_SECRET!
 
 const app = new cdk.App();
 new WebhookStack(app, 'WebhookStack', {
