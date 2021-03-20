38615196801f710c1321f9340d1605697b33b5d1
nya <yuki.nakamura.jpn@gmail.com>
2021-03-20 11:00:35 +0900

    Add lint

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,11 +1,10 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { WebhookStack } from '../lib/webhook';
+import 'source-map-support/register'
+import * as cdk from '@aws-cdk/core'
+import { WebhookStack } from '../lib/webhook'
 import { join } from 'path'
 
 require('dotenv').config()
-
 
 const lambdaLayerDir = join(process.cwd(), '../cdk/dist/layer')
 const lambdaCodeDir = join(process.cwd(), '../cdk/lambda')
@@ -13,10 +12,10 @@ const lambdaCodeDir = join(process.cwd(), '../cdk/lambda')
 const lineChannelAccessToken = process.env.LINE_CHANNEL_ACCESS_TOKEN!
 const lineChannelSecret = process.env.LINE_CHANNEL_SECRET!
 
-const app = new cdk.App();
+const app = new cdk.App()
 new WebhookStack(app, 'WebhookStack', {
-    lambdaCodeDir,
-    lambdaLayerDir,
-    lineChannelAccessToken,
-    lineChannelSecret
-});
+	lambdaCodeDir,
+	lambdaLayerDir,
+	lineChannelAccessToken,
+	lineChannelSecret,
+})
